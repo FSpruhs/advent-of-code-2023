f9265f17477637bfb34dcace189d5cc38310ff97
FSpruhs <spruhs.com>
2023-11-30 00:09:04 +0100

    first commit

---
+++ util/util.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func BuildFilePath(day int, example bool) string {
+	if example {
+		return fmt.Sprintf("./day%d/example.txt", day)
+	}
+	return fmt.Sprintf("./day%d/puzzle.txt", day)
+}
+
+func Read(path string) []string {
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("error %s while reading file %s", err, path)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		zeile := scanner.Text()
+		lines = append(lines, zeile)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("error %s while scanning file %s", err, path)
+		os.Exit(1)
+	}
+
+	return lines
+}
